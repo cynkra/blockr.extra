35e39e5dd83f5d0782f35312da09402f1e6db71b
Christoph Sax <christoph.sax@gmail.com>
2026-01-23 10:12:36 +0100

    session wf

--- dev/examples/function_block_demo.R
+++ dev/examples/function_block_demo.R
@@ -1,10 +1,33 @@
 # Load required libraries
 library(blockr)
-library(blockr.extra)
+# library(blockr.extra)
 pkgload::load_all()
 
+library(blockr.session)
+
+options(
+  "g6R.layout_on_data_change" = TRUE
+)
+
+# Unregister all blockr.core blocks except dataset_block
+# (other packages like blockr.io, blockr.dplyr, blockr.ggplot provide better alternatives)
+blockr.core::unregister_blocks(c(
+  "subset_block",
+  "merge_block",
+  "rbind_block",
+  "head_block",
+  "scatter_block",
+  "upload_block",
+  "filebrowser_block",
+  "csv_block",
+  "static_block",
+  "glue_block"
+))
+
+
+
 # Demo workflow for function_block
-run_app(
+board <- new_dock_board(
   blocks = c(
     data = new_dataset_block(dataset = "iris"),
 
@@ -30,5 +53,12 @@ run_app(
   links = c(
     new_link("data", "head_fn", "data"),
     new_link("data", "summary_fn", "data")
+  ),
+  extensions = list(
+    blockr.dag::new_dag_extension()
   )
 )
+
+
+# Serve with manage_project plugin for workflow save/load
+serve(board, plugins = custom_plugins(manage_project()))
